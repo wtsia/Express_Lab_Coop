8f39c3b5e243e1a87ada96d470e263749d308f14
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-09-26 17:11:55 -0400

    changed up order to get rid of error

--- controllers/myItems.js
+++ controllers/myItems.js
@@ -2,10 +2,41 @@ const express = require('express');
 const router = express.Router();
 const myModel = require('../models/meme');
 
+router.post('/', (req, res) => {
+    myModel.create(req.body)
+    .then(newItem => {
+        res.redirect('/');
+    }).catch(err => {
+        console.log(err);
+    }
+    )
+});
+
+router.put('/:id', (req, res) => {
+    myModel.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+    .then(instance =>{
+        res.redirect('/')
+}).catch(err => {
+    console.log(err);
+}
+)
+});
 
 router.get('/', (req, res) => {
     myModel.find({}).then(instances => res.render('index', {instances}));
 })
+
+router.get('/edit/:id', (req, res) =>
+{
+    myModel.findOne({_id: req.params.id})
+    .then(instance => {
+        res.render('edit', {myInstance})
+    }).catch(err => {
+        console.log(err);
+    }
+    )
+})
+
 
 
 router.get('/new', (req, res) => {
@@ -17,35 +48,9 @@ router.get("/:id", (req, res) => {
 });
 
 
-router.get('/edit/id', (req, res) =>
-{
-    myModel.findOne({_id: req.params.id})
-    .then(instance => {
-        res.render('edit', {myInstance})
-    }).catch(err => {
-        console.log(err);
-    }
-    )
-})
-router.post('/', (req, res) => {
-    myModel.create(req.body)
-    .then(newItem => {
-        res.redirect('/');
-    }).catch(err => {
-        console.log(err);
-    }
-    )
-})
 
-router.put('/:id', (req, res) => {
-    myModel.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
-    .then(instance =>{
-        res.redirect('/')
-}).catch(err => {
-    console.log(err);
-}
-)
-});
+
+
 
 
 
